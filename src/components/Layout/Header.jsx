6ae028e91db4e609d5d42a7d42868c2b313e9e72
afalarco22 <afalarco@unal.edu.co>
2022-11-24 15:39:09 -0500

    Agregando logo al encabezado

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import logo from '../../Img/logo.jpg'
 
 export default function Header() {
   return (
     <header className="encabezado">
+        <img src={logo} />
       <nav className="navbar navbar-expand-lg barra-navegacion ">
         <article className="container-fluid">
           <aside className="collapse navbar-collapse contenedor-enlaces" id="navbarNav">
--- src/components/helpers/FormularioCliente.jsx
+++ src/components/helpers/FormularioCliente.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import Input from './Input'
 
 export default function FormularioCliente() {
   return (
     <aside className='contenedor-fomulario'>
         <form className='formulario-cliente'>
-          <label className='label-nombre'>Nombre del cliente</label>
+          <label className='label-nombre'>{"Nombre del cliente"}</label>
           <Input placeholder='Nombre'/>
 
-          <label className='label-documento'>Documento</label>
+          <label className='label-documento'>{"Documento"}</label>
           <Input placeholder='Documento' />
 
-          <label className='label-correo'>Correo</label>
+          <label className='label-correo'>{"Correo"}</label>
           <Input placeholder='Correo' />
+
+          <label className='label-correo'>{"Direccion"}</label>
+          <Input placeholder='Direccion' />
+
+          <label className='label-correo'>{"Barrio"}</label>
+          <Input placeholder='Barrio' />
+
+          <label className='label-correo'>{"Telefono Contacto"}</label>
+          <Input placeholder='Telefono' />
 
         </form>
     </aside>
